657d497c8f5e347ead7ec4aebf01102fbac27e62
Webmastee <103311266+Webmastee@users.noreply.github.com>
2022-09-01 03:33:59 +0100

    Add files via upload

---
+++ video/app.js
@@ -0,0 +1,26 @@
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const videoContainer = document.querySelector(".video-container");
+const switchBtn = document.querySelector(".switch-btn");
+
+switchBtn.addEventListener("click", function () {
+    if (!switchBtn.classList.contains("slide")) {
+        switchBtn.classList.add("slide");
+        videoContainer.pause();
+    }
+    else {
+        switchBtn.classList.remove("slide");
+        videoContainer.play();
+    }
+})
+
+
+// preloader
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+    preloader.classList.add("hide-preloader");
+})
